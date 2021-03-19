3471182914f2692cd063bfcd5a23b672e967f616
yinyichao <yinyichao@163.com>
2021-03-19 17:30:24 +0800

    task05 add readme

--- Week-01/task05/README.md
+++ Week-01/task05/README.md
@@ -1,4 +1,4 @@
-# JAVA-002
-极客大学Java进阶训练营2期1班
+G1 gc不是特别了解，不知道从何入手进行分析
 
-殷一超作业
+![图片](https://uploader.shimo.im/f/ebrx4MvfsMko4Kgd.png!thumbnail?fileGuid=Yw93TJgcJgYvgrrV)
+
---
+++ Week-01/task05/a.java
@@ -0,0 +1,10 @@
+
+
+public class a {
+    public static void main(String[] args) {
+        int a = 1;
+        for(int i =0;i<5;i++){
+            a++;
+        }
+    }
+}
