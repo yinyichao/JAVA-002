b98029360cf4c98aa1111abb1641151ca2091a3c
yinyichao <yinyichao@163.com>
2021-04-01 14:07:41 +0800

    finish week03-task02

---
+++ Week-03/task01/task01/HttpClient.java
@@ -0,0 +1,55 @@
+/**
+ * @ClassName HttpClient
+ * @Description TODO
+ * @Author yins
+ * @Date 2021-3-29
+ * @Version 1.0
+ **/
+package com.yins.week_03.task01;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClient {
+    public static String get(String url){
+        String msg = "";
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response = null;
+        try{
+            // 配置信息
+            RequestConfig requestConfig = RequestConfig.custom()
+                    // 设置连接超时时间(单位毫秒)
+                    .setConnectTimeout(5000)
+                    // 设置请求超时时间(单位毫秒)
+                    .setConnectionRequestTimeout(5000)
+                    // socket读写超时时间(单位毫秒)
+                    .setSocketTimeout(5000)
+                    // 设置是否允许重定向(默认为true)
+                    .setRedirectsEnabled(true).build();
+            // 将上面的配置信息 运用到这个Get请求里
+            httpGet.setConfig(requestConfig);
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+            // 从响应模型中获取响应实体
+            HttpEntity entity = response.getEntity();
+            msg = EntityUtils.toString(entity);
+            System.out.println("响应内容为:" + msg);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return msg;
+    }
+}
--- Week-03/task02/task02/netty/HttpHandler.java
+++ Week-03/task02/task02/netty/HttpHandler.java
@@ -2,12 +2,12 @@
  * @ClassName HttpHandler
  * @Description TODO
  * @Author yins
- * @Date 2021-3-29
+ * @Date 2021-3-30
  * @Version 1.0
  **/
-package com.yins.week_03.task01.netty;
+package com.yins.week_03.task02.netty;
 
-import com.yins.week_03.task01.HttpClient;
+import com.yins.week_03.task02.NettyHttpClient;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
@@ -18,6 +18,8 @@ import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpUtil;
 import io.netty.util.ReferenceCountUtil;
 
+import java.io.UnsupportedEncodingException;
+
 import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
 import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
 import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
@@ -26,19 +28,20 @@ import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class HttpHandler extends ChannelInboundHandlerAdapter {
     @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) {
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         ctx.flush();
     }
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         try {
             //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
             FullHttpRequest fullRequest = (FullHttpRequest) msg;
             String uri = fullRequest.uri();
-            //logger.info("接收到的请求url为{}", uri);
-            if (uri.contains("/test")) {
-                handlerTest(fullRequest, ctx);
+            if(uri.contains("/test")){
+                handlerTest(fullRequest,ctx);
+            }else{
+                handlerTest(fullRequest,ctx);
             }
 
         } catch(Exception e) {
@@ -47,29 +50,25 @@ public class HttpHandler extends ChannelInboundHandlerAdapter {
             ReferenceCountUtil.release(msg);
         }
     }
-    protected final static String url = "http://127.0.0.1:8803";
+
     private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
         FullHttpResponse response = null;
+        NettyHttpClient client = new NettyHttpClient("127.0.0.1",8803);
+        String value = client.action();
+        System.out.println(value);
         try {
-            //String value = "hello,netty"; // "hello,kimmking"; // 对接上次作业的httpclient或者okhttp请求另一个url的响应数据
-            String value = HttpClient.get(url);
-//            httpGet ...  http://localhost:8801
-//            返回的响应，"hello,nio";
-//            value = reponse....
-
-            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response = new DefaultFullHttpResponse(HTTP_1_1,OK,Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
             response.headers().set("Content-Type", "application/json");
-            response.headers().setInt("Content-Length", response.content().readableBytes());
-
-        } catch (Exception e) {
-            System.out.println("处理出错:"+e.getMessage());
+            response.headers().setInt("Content-Length",value.length());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
             response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
-        } finally {
-            if (fullRequest != null) {
-                if (!HttpUtil.isKeepAlive(fullRequest)) {
+        }finally {
+            if(fullRequest != null){
+                if(!HttpUtil.isKeepAlive(fullRequest)){
                     ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-                } else {
-                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                }else{
+                    response.headers().set(CONNECTION,KEEP_ALIVE);
                     ctx.write(response);
                 }
             }
@@ -77,7 +76,7 @@ public class HttpHandler extends ChannelInboundHandlerAdapter {
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
         ctx.close();
     }
